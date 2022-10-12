7a00b886f7c1bb60a2b975ddc86d7f549697b768
Semyon <tonkih.sem@mail.ru>
2022-10-12 22:07:28 +0500

    added task40

--- task39/Program.cs
+++ task39/Program.cs
@@ -4,3 +4,44 @@
 [1 2 3 4 5] -> [5 4 3 2 1]
 [6 7 3 6] -> [6 3 7 6]
 */
+
+int[] generateArray(int length, int start, int end)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(start, end + 1);
+    }
+    return array;
+}
+
+void printArray(int[] arrayToPrint, string name)
+{
+    Console.WriteLine($"--------------{name}--------------");
+    Console.Write("[");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+
+        Console.Write(arrayToPrint[i]);
+        if (i < arrayToPrint.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine("]");
+    Console.WriteLine();
+}
+
+int[] reverseArray(int[] inputArray)
+{
+    int[] reversedArray = new int[inputArray.Length];
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        reversedArray[i] = inputArray[inputArray.Length - i - 1];
+    }
+    return reversedArray;
+}
+int[] currentArray = generateArray(9, 10, 99);
+printArray(currentArray, "Прямой");
+int[] reversedArray = reverseArray(currentArray);
+printArray(reversedArray, "Обратный");
---
+++ task40/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли 
+существовать треугольник с сторонами такой длины.
+Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+*/
+
+int getNumberFromUser(string userInformation)
+{
+    int result = 0;
+    while (result == 0)
+    {
+        Console.Write(userInformation);
+        string userLine = Console.ReadLine();
+        int.TryParse(userLine, out result);
+        if (result == 0 && userLine != "0")
+        {
+            Console.WriteLine($"Ошибка ввода! Введите целое число: [{userLine}].");
+        }
+        else
+        {
+            break;
+        }
+    }
+    return result;
+}
+
+void checkUp(int numA, int numB, int numC)
+{
+    if (numA < numB + numC)
+    {
+        if (numB < numA + numC)
+        {
+            if (numC < numA + numB)
+            {
+                Console.WriteLine($"Треугольник со сторонами [{numA}], [{numB}] и [{numC}] может существовать");
+            }
+            else
+            {
+                Console.WriteLine($"Треугольник со сторонами [{numA}], [{numB}] и [{numC}] не может существовать");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Треугольник со сторонами [{numA}], [{numB}] и [{numC}] не может существовать");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Треугольник со сторонами [{numA}], [{numB}] и [{numC}] не может существовать");
+    }
+}
+
+int userNumA = getNumberFromUser("Введите число A: ");
+int userNumB = getNumberFromUser("Введите число B: ");
+int userNumC = getNumberFromUser("Введите число C: ");
+checkUp(userNumA, userNumB,userNumC);
