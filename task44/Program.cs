4547adbc2f3faa8d51a9275c2188bbf71765cf5b
Semyon <tonkih.sem@mail.ru>
2022-10-15 16:51:34 +0500

    added task 44

--- task44/Program.cs
+++ task44/Program.cs
@@ -5,3 +5,53 @@
 Если N = 7 -> 0 1 1 2 3 5 8
 */
 
+int[] FibonacciN(int numberN)
+{
+    int[] array = new int[numberN];
+    array[0] = 0;
+    array[1] = 1;
+    for (int i = 2; i < array.Length; i++)
+    {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+    return array;
+}
+
+void printArray(int[] incomingArray) //печатаем массив
+{
+    Console.Write("[");
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        Console.Write(incomingArray[i]);
+        if (i < incomingArray.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+int getNumberFromUser(string userInformation)
+{
+    int result = 0;
+    while (result == 0)
+    {
+        Console.Write(userInformation);
+        string userLine = Console.ReadLine();
+        int.TryParse(userLine, out result);
+        if (result == 0 && userLine != "0")
+        {
+            Console.WriteLine($"Ошибка ввода! Введите целое число: [{userLine}].");
+        }
+        else
+        {
+            break;
+        }
+    }
+    return result;
+}
+
+int number = getNumberFromUser("Введите N: ");
+int [] result = FibonacciN(number);
+printArray(result);
